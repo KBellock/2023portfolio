c93b8a3f4d54022de659cffd4b3c81371b875443
kevinbellock <kevinbellock@gmail.com>
2023-08-15 15:41:43 -0600

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <div>
+      ©2023 Kevin Bellock All rights reserved.
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
