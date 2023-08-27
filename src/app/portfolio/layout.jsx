807782bec4041eb29f0c30310ca468660b59d888
kevinbellock <kevinbellock@gmail.com>
2023-08-27 16:38:25 -0600

    finished filler content

--- src/app/portfolio/layout.jsx
+++ src/app/portfolio/layout.jsx
@@ -6,6 +6,7 @@ function Layout({children}) {
   return (
     <div>
       <h1 className={styles.maintitle}>My Work - View my Github <a href="https://github.com/KBellock" target="_blank" className='link'>Here</a></h1>
+      <h2>THIS PAGE IS UNDER CONSTRUCTION</h2>
       {children}
     </div>
   )
--- src/app/portfolio/websites/page.jsx
+++ src/app/portfolio/websites/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Card from '@/components/card/Card'
+import websites from './websites'
 
-function page() {
+function Websites() {
   return (
     <div>
-      <h1>Websites</h1>
+      <h1>React Native Projects</h1>
+        <div>
+            {websites.map((website) => (
+                <Card application={website} key={website.id} />
+            ))}
+        </div>
     </div>
   )
 }
 
-export default page
+export default Websites
